51b943e0600fbaf172cbf7ecb5c567ce2b04e6e7
Andrey Budovsky <uyean@yandex.ru>
2023-06-07 14:01:39 +0300

    v1.03
    Services и Portfolio

---
+++ src/js/components/sliders.js
@@ -0,0 +1,17 @@
+import _vars from '../_vars';
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+Swiper.use([Navigation, Pagination]);
+
+const bodyStyle = window.getComputedStyle(document.body);
+const gap = parseInt(bodyStyle.getPropertyValue('--grid-gap'));
+
+const portfolioSlider = new Swiper('.portfolio-section__items', {
+  slidesPerView: 3,
+  spaceBetween: gap,
+  loop: true,
+    navigation: {
+    nextEl: '.portfolio-section__next',
+    prevEl: '.portfolio-section__prev',
+  },
+});
